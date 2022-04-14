fada4683c8c16e96f3e29da9f603b72a1328ee95
Mueid <mrmuyed579@gmail.com>
2022-04-14 23:40:09 +0600

    shipping info done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import './Cart.css';
 
 const Cart = (props) => {
     const { cart } = props;
-    console.log(props.children);
+    //console.log(props.children);
     let total = 0;
     let shipping = 0;
     let quantity = 0;
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+
 
 const Shipment = () => {
+    const [user] = useAuthState(auth);
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const [error, setError] = useState('');
+
+    const heandleNameBlur = event =>{
+        setName(event.target.value);
+    }
+
+    
+    const heandleAddressBlur = event => {
+        setAddress(event.target.value);
+    }
+
+    const heandelPhoneNumberBlur = event => {
+        setPhone(event.terget.value);
+    }
+    
+    
+
+    const heandleCreateUser = event => {
+        event.preventDefault();
+        const shipping={ email, name, address, phone}
+        console.log(shipping);
+    }
     return (
-        <div>
-            <h1>this is shipment</h1>
+        <div className='form-container'>
+            <div>
+                <h1 className='from-title'>Shipping Information</h1>
+                <form onSubmit={heandleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="name">Name</label>
+                        <input onBlur={heandleNameBlur} type="text" name="name" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input value={user?.email} readOnly type="email" name="email" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="address">Address</label>
+                        <input onBlur={heandleAddressBlur} type="text" name="address"  required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="phone">Phone Number</label>
+                        <input onBlur={heandelPhoneNumberBlur} type="text" name="phone" required />
+                    </div>
+                    <p style={{ color: 'red' }} > {error}</p>
+                    <input className='form-submit' type="submit" value="Add Shipping"></input>
+                </form>
+
+                <p>Already have an Account? <Link className='form-link' to='/login'>Login</Link> </p>
+            </div>
         </div>
     );
 };
